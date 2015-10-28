8afe6c4964709a15f5b658c98850219cfe576744
Edward Suczewski <ed.suczewski@gmail.com>
2015-10-28 17:29:07 -0600

    Finished Except Black Diamond
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,8 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+        
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if let text = searchBar.text {
+            WeatherController.sharedInstance.searchWeather(text, completion: { (success) -> Void in
+                if success {
+                    if let weather = WeatherController.sharedInstance.weatherArray.last {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.mainLabel.text = weather.main
+                            self.descriptionLabel.text = weather.description
+                            if let temp = weather.temp {
+                                self.tempLabel.text = String(temp)
+                            }
+                            self.cityNameLabel.text = weather.name
+                            
+                        })
+                    }
+                }
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NetworkController.dataAtURL(NetworkController.searchURL(String(20171))) { (resultData) -> Void in
